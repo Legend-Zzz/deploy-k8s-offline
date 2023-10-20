e28a83db23f1a8439acc636101ac960ca6bf8743
legendzzzaioi <legendzzzaioi@gmail.com>
2023-10-20 16:35:58 +0800

    add support debian12 and ubuntu

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # deploy-k8s-offline
 Deploy K8S cluster offline version 1.27.5 with ansible, certificate is valid for 200 years
 
-Based on centos7 or debian11
+Based on debian11/12 or ubuntu20.04/22.04 or centos7
 ### usage
 modify inventory:  .ansible/hihosts
 
--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -10,10 +10,14 @@ if [ -f /etc/os-release ]; then
 
   case $ID in
     "debian")
+      if [ "$VERSION_ID" != "11" ] && [ "$VERSION_ID" != "12" ]; then
+          echo "Unsupported version: $VERSION_ID"
+          exit 1
+      fi
       if ! command -v sudo &> /dev/null; then
         echo "主机未安装sudo，请输入root用户密码进行安装："
         read -s password
-        if ! echo $password | su -c "dpkg -i ./package/sudo/*.deb" &>/dev/null; then
+        if ! echo $password | su -c "dpkg -i ./package/sudo-debian$VERSION_ID/*.deb" &>/dev/null; then
           echo "密码错误或安装失败，程序退出"
           exit 1
         fi
@@ -31,12 +35,19 @@ if [ -f /etc/os-release ]; then
       fi
 
       if ! command -v ansible &>/dev/null; then
-        sudo dpkg -i ./package/ansible-debian/*.deb
+        sudo dpkg -i ./package/ansible-debian"$VERSION_ID"/*.deb
+      fi
+      if ! command -v kubelet &>/dev/null; then
+        sudo dpkg -i ./package/kubelet-debian"$VERSION_ID"/*.deb
       fi
 
       sudo sed -i 's#interpreter_python=/usr/bin/python2#interpreter_python=/usr/bin/python3#g' .ansible/ansible.cfg
       ;;
-    "centos")
+    "centos" | "rhel")
+      if [ "$VERSION_ID" != "7" ]; then
+        echo "Unsupported version: $VERSION_ID"
+        exit 1
+      fi
       if ! sudo -v &> /dev/null; then
         if [ ! -v password ]; then
           echo "当前用户$USER没有sudo权限，请输入root用户密码为当前用户配置权限："
@@ -49,8 +60,37 @@ if [ -f /etc/os-release ]; then
       fi
 
       if ! command -v ansible &>/dev/null; then
-        sudo yum localinstall -y ./package/ansible-centos/*.rpm
+        sudo yum localinstall -y ./package/ansible-centos"$VERSION_ID"/*.rpm
       fi
+      if ! command -v kubelet &>/dev/null; then
+        sudo yum localinstall -y ./package/kubelet-centos"$VERSION_ID"/*.rpm
+      fi
+      ;;
+    "ubuntu")
+      if [ "$VERSION_ID" != "20.04" ] && [ "$VERSION_ID" != "22.04" ]; then
+          echo "Unsupported version: $VERSION_ID"
+          exit 1
+      fi
+
+      if ! sudo -v &> /dev/null; then
+        if [ ! -v password ]; then
+          echo "当前用户$USER没有sudo权限，请输入root用户密码为当前用户配置权限："
+          read -s password
+        fi
+        if ! echo $password | su -c 'echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers' &>/dev/null; then
+          echo "密码错误或配置失败，程序退出"
+          exit 1
+        fi
+      fi
+
+      if ! command -v ansible &>/dev/null; then
+        sudo dpkg -i ./package/ansible-ubuntu"$VERSION_ID"/*.deb
+      fi
+      if ! command -v kubelet &>/dev/null; then
+        sudo dpkg -i ./package/kubelet-ubuntu"$VERSION_ID"/*.deb
+      fi
+
+      sudo sed -i 's#interpreter_python=/usr/bin/python2#interpreter_python=/usr/bin/python3#g' .ansible/ansible.cfg
       ;;
     *)
       echo "Unsupported operating system: $ID"
